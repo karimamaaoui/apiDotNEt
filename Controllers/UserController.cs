667fc71ce0b98e3a7266f0622aa53a1e138f7c7a
karimamaaoui <maaouika07@gmail.com>
2023-08-02 15:06:58 +0200

    user profile commit

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -62,7 +62,7 @@ namespace CoolApi.Controllers
             new Claim("role", user.role.ToString()),
             new Claim("refreshToken", user.RefreshToken),
         }),
-                Expires = DateTime.UtcNow.AddDays(1),
+                Expires = DateTime.UtcNow.AddMinutes(1),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
 
